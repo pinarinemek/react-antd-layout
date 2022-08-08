1f9d728e3acfb3792fa387b1097f103dc1559abb
pinarinemek <pinarinemek@gmail.com>
2022-08-08 21:48:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Title from 'antd/lib/typography/Title';
+import { Layout , Avatar } from 'antd';
+import SubMenu from 'antd/lib/menu/SubMenu';
+import { Menu , Breadcrumb } from 'antd';
+
+
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Layout>
+     <Header style={{padding : 10 }}>
+     <Avatar style={{float : 'right' }} src= './dog.png' />
+     <Title style = {{color : 'white' , paddingLeft: 20}} level={3}>Team 8</Title>
+     </Header >
+     <Layout> <Sider >
+     <Menu>
+<Menu.Item key= 'dashboard' >
+  Dashboard
+</Menu.Item>
+<SubMenu>
+  <Menu.ItemGroup key= 'about US' Title = 'About US'>
+    <Menu.Item key= 'location 1' > Location 1 </Menu.Item>
+    <Menu.Item key= 'location 2' > Location 2 </Menu.Item>
+  </Menu.ItemGroup>
+</SubMenu>
+     </Menu>
+
+     </Sider> 
+     <Layout>
+        
+         <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+      <Breadcrumb.Item>Home</Breadcrumb.Item>
+      <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+        <Breadcrumb.Item>Dashboard2</Breadcrumb.Item>
+       
+      </Breadcrumb>
+      <div style={{background : '#fff' , padding : 24 , minHeight : 580 }}>Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design Exampla Created by pinar </Footer>
+      </Layout>
+     </Layout>
+      
+     
+    </Layout>
     </div>
   );
 }
